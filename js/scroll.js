ba0670341cd687219b7300cd16533be63090d44f
Abashkina <88579388+YevgeniiaAbashkina@users.noreply.github.com>
2023-09-22 09:35:08 +0200

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -1,1 +1,42 @@
+/** 
+* Edit navigation to scroll event
+*/
 
+
+function getCoords( elem ) {
+    var box = elem.getBoundingClientRect();
+
+    return {
+        top:    box.top + window.scrollY,
+        bottom: box.bottom + window.scrollY
+    };
+};
+
+window.addEventListener( 'scroll', function() {
+
+    var burger    = document.querySelector( '.trigger' );
+    var navi      = document.querySelector( '.navigation' );
+    var naviMoove = document.querySelector( '.navigation__moove' );
+
+    var header   = document.querySelector( '.header' );
+    var about    = document.querySelector( '.about' );
+
+
+    if( ( getCoords( navi ).bottom - 40 ) < getCoords( about ).top ) {
+        navi.style.opacity = '1';
+
+        header.classList.remove( 'moove' );
+        burger.classList.add( 'remove' ); 
+        naviMoove.style.display = 'none';
+        navi.style.pointerEvents = 'all';
+    }
+    
+    else {
+        navi.style.opacity = '0';
+
+        header.classList.add( 'moove' );
+        burger.classList.remove( 'remove' ); 
+        naviMoove.style.display = 'inline-block';
+        navi.style.pointerEvents = 'none';
+    };
+} );
